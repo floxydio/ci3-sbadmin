bdd621e2d716b674cf040ebf0535569a97daed73
floxydio <63448336+floxydio@users.noreply.github.com>
2020-11-11 12:56:52 +0700

    Masih Tahap Pengerjaan - Nanti Dilanjut

---
+++ application/controllers/User.php
@@ -0,0 +1,10 @@
+<?php 
+
+
+class User extends CI_Controller {
+
+	public function index() {
+		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+		echo 'Selamat Datang User ' . $data['user']['name'];
+	}
+}
