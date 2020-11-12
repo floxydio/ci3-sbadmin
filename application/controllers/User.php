8f0d3e63b8e47a43ac97040d078e326c45bfd34b
Dio Rovelino <63448336+floxydio@users.noreply.github.com>
2020-11-12 11:29:21 +0700

    Redirect to load view

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,7 +4,12 @@
 class User extends CI_Controller {
 
 	public function index() {
+		$data['title'] = 'Dashboard';
 		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-		$this->load->view('user/index');
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/sidebar', $data);
+		$this->load->view('templates/topbar', $data);
+		$this->load->view('user/index', $data);
+		$this->load->view('templates/footer');
 	}
 }
