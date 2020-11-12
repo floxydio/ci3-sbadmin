6e30370bc7aee311a4c0a8ced037423510394dfd
Dio Rovelino <63448336+floxydio@users.noreply.github.com>
2020-11-12 13:32:00 +0700

    Update Controller -> Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,8 +1,18 @@
-<?
-
-class Admin extends CI_controller 
-{
+<?php 
 
 
-	
+class Admin extends CI_Controller {
+
+
+		public function index() {
+		$data['title'] = 'Dashboard';
+		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/sidebar', $data);
+		$this->load->view('templates/topbar', $data);
+		$this->load->view('adminarea/index', $data);
+		$this->load->view('templates/footer');
+	}
+
+
 }
